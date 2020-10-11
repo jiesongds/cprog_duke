3bd25bb99f7b2fbd1eaf3fa1e6ec01ed7b1c6af9
Coursera Learner <nobody@nowhere.nul>
2020-10-11 12:20:02 +0000

    05_squares

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -9,6 +9,10 @@
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
   // else, return 0
+  int upper_bd = offset + size; 
+  if (coord >= offset && coord < upper_bd){
+    return 1; 
+  }
   return 0;
 }
 
@@ -19,18 +23,35 @@ int isInRange(int coord, int offset, int size) {
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
   // return 1, else return 0
+  int upper_bd = offset + size;
+  if (coord == offset || coord == upper_bd){
+    return 1; 
+  }
   return 0;
 }
 
+int max(int x, int y){
+  if (x > y){
+    return x;
+  }
+  else{
+    return y;
+  }
+}
+
+
+
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+  int bd1 = x_offset + size2;
+  int w = max(size1, bd1);
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int bd2 = y_offset + size2;
+  int h = max(size1, bd2); 
   //count from 0 to h. Call the number you count with y
-
+  for (int y  = 0; y<h; y++){
     //count from 0 to w. Call the number you count with x
-
+    for (int x = 0; x<w; x++){
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -38,16 +59,38 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
+      int xrange = isInRange(x, x_offset, bd1);
+      int yborder = isAtBorder(y, y_offset, size2 - 1);
+      int yrange = isInRange(y, y_offset, bd2);
+      int xborder = isAtBorder(x, x_offset, size2 -1);
 
+      int y_minus = size1 - 1;
+      int x_minus = size1 - 1;
+      if (xrange==1 && yborder ==1){
+	printf("*");
+      }
+      else if (yrange==1 && xborder==1){
+	printf("*");
+      }
       //if not,
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-
+      else if (x < size1 && (y == 0 || y == y_minus)){
+	  printf("#");
+	}
+      else if (y < size1 && (x == 0 || x == x_minus)){
+	  printf("#");
+	}
+      else{
+	printf(" ");
+      }
+      }
+    printf("\n");
       //else print a space
     //when you finish counting x from 0 to w, 
     //print a newline
-
+    }
 }
