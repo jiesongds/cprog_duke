73ed8d3d6276471de72e8ee9766cc5f6608dd8f1
Jie Song <songjay8@gmail.com>
2020-11-28 10:21:40 +0800

    ast 25 finished

--- 25_break_encr/break.c
+++ 25_break_encr/break.c
@@ -2,9 +2,6 @@
 #include <stdio.h>
 #include <ctype.h>
 
-
-
-
 int main(int argc, char ** argv) {
 
     if (argc != 2) {
@@ -19,8 +16,51 @@ int main(int argc, char ** argv) {
         return EXIT_FAILURE;
     }
 
+    char letters[26] = "abcdefghijklmnopqrstuvwxyz"; 
+
+    int freq_count[26] = {0}; 
+
+    int c; 
+
+    while ((c = fgetc(f)) != EOF)
+    {
+        for (int i = 0; i<26; i++) {
+            if (letters[i] == c) {
+                freq_count[i]++;
+                break; 
+            }
+        }
+    }
     
+    fclose(f); 
 
 
+    //for (int i =0; i<26; i++) {
+        //printf("%d\n", freq_count[i]);
+    //}
+
+    int max_count = freq_count[0];
+    
+    for (int j = 1; j<26; j++) {
+        if (freq_count[j] > max_count) {
+            max_count = freq_count[j]; 
+        }
+    }
+    int max_index; 
+    for (int i = 0; i<26; i++) {
+        if (freq_count[i] == max_count) {
+            max_index = i;
+            break;
+        }
+    }
+
+    //printf("Max count is %d\n", max_count); 
+    //printf("Max index is %d\n", max_index); 
+
+    int key = (max_index + 22) % 26; 
+
+    printf("%d\n", key); 
+
+    return EXIT_SUCCESS; 
 
 }
