e0897a1550ccdfb0788843cd37f161b49d0d0610
Jie Song <songjay8@gmail.com>
2020-11-28 11:32:56 +0800

    copied work

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -1,67 +1,44 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
-int main(int argc, char ** argv) {
-
-    if (argc != 2) {
-        fprintf(stderr, "Usage: wrong number of input parameters");
-        return EXIT_FAILURE;
+void getFreqArray(int *freqArr, FILE * f){
+  int c;
+  while((c=fgetc(f)) != EOF){
+    if(isalpha(c)){
+      c = tolower(c);
+      c = c - 'a';
+      freqArr[c] = freqArr[c]+1;
     }
+  }
+}
 
-    FILE * f = fopen(argv[1], "r");
-
-    if (f == NULL) {
-        perror("Could not open file");
-        return EXIT_FAILURE;
+int maxFreqIdx(int * freqArr){
+  int max = freqArr[0];
+  int maxIdx = 0;
+  for(int i=0; i<26; i++){
+    if(freqArr[i]>max){
+      max = freqArr[i];
+      maxIdx = i;
     }
+  }
+  return maxIdx;
+}
 
-    //char letters[26] = "abcdefghijklmnopqrstuvwxyz"; 
-
-    int freq_count[26] = {0}; 
-
-    int c; 
-
-    int counter = 0;
-    
-    while ((c = fgetc(f)) != EOF && counter <=500000)
-    {
-      if (!isalpha(c) || !islower(c)){
-	continue;
-      }
-      freq_count[c-97]++;
-      counter++;
-    }
-    
-    fclose(f); 
-
-
-    //for (int i =0; i<26; i++) {
-        //printf("%d\n", freq_count[i]);
-    //}
-
-    int max_count = freq_count[0];
-    int max_index = 0;
-    
-    for (int j = 1; j<26; j++) {
-        if (freq_count[j] > max_count) {
-            max_count = freq_count[j];
-	    max_index = j;
-        }
-    }
-
-    //printf("Max count is %d\n", max_count); 
-    //printf("Max index is %d\n", max_index); 
-
-    int key = (max_index + 22) % 26; 
-
-    if (key <0 || key >= 26) {
-      fprintf(stderr, "Wrong Key\n");
-      return EXIT_FAILURE;
-    }
-    
-    printf("%d\n", key); 
-
-    return EXIT_SUCCESS; 
-
+int main(int argc, char ** argv){
+  if(argc!=2){
+    fprintf(stderr, "Usage: Wrong amount of Arguments\n");
+    return EXIT_FAILURE;
+  }
+  FILE * f = fopen(argv[1],"r");
+  if(f==NULL){
+    perror("Could not open file");
+    return EXIT_FAILURE;
+  }
+  int freqArr[26] = {0};
+  getFreqArray(freqArr, f);
+  int maxIdx = maxFreqIdx(freqArr);
+  int key = (maxIdx + 'a' - 'e' + 26)%26;
+  printf("%d\n", key);
 }
