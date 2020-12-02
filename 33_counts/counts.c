b9ee34e153096c1ae893e9d31f087e98fd75a4a0
Jie Song <songjay8@gmail.com>
2020-12-02 15:25:42 +0800

    did ast 33

--- 33_counts/counts.c
+++ 33_counts/counts.c
@@ -4,14 +4,71 @@
 #include "counts.h"
 counts_t * createCounts(void) {
   //WRITE ME
+  counts_t* ctarray; 
+  ctarray = malloc(sizeof(*ctarray)); 
+  ctarray->ct = NULL;
+  ctarray->length = 0;
+  ctarray->unknown_ct = 0;
+  return ctarray; 
+}
+
+ssize_t find_name_idx(counts_t * c, const char * name) {
+  if (c->ct == NULL) {
+    return -1; 
+  }
+  else {
+    for (size_t i=0; i<c->length; i++) {
+      char * checkname = c->ct[i]->value; 
+      if (strcmp(checkname, name) == 0) {
+        return i; 
+      }
+    }
+  }
+  return -1; 
 }
 void addCount(counts_t * c, const char * name) {
   //WRITE ME
+  if (name == NULL) {
+    c->unknown_ct++;
+    return; 
+  }
+  ssize_t i = find_name_idx(c, name); 
+  
+  if (i < 0 ) {
+    size_t name_lg = strlen(name) + 1;
+    c->ct = realloc(c->ct, (c->length + 1) * sizeof(*(c->ct)));
+    c->ct[c->length] = malloc(sizeof(**(c->ct)));
+    c->ct[c->length]->value = malloc(name_lg * sizeof(char)); 
+    strcpy(c->ct[c->length]->value, name);
+    c->ct[c->length]->count = 1;
+    c->length++;
+  }
+  else {
+    c->ct[i]->count++; 
+  }
 }
 void printCounts(counts_t * c, FILE * outFile) {
   //WRITE ME
+  for (size_t i=0; i<c->length; i++) {
+    fprintf(outFile, "%s: %ld\n", c->ct[i]->value, c->ct[i]->count);
+  }
+  if (c->unknown_ct >0) {
+    fprintf(outFile, "<unknown>: %ld\n", c->unknown_ct);
+  }
 }
 
 void freeCounts(counts_t * c) {
   //WRITE ME
+  if (c->ct == NULL) {
+    free(c);
+    return;
+  }
+  for (size_t i=0; i<c->length; i++) {
+    free(c->ct[i]->value);
+  }
+  for (size_t i=0; i<c->length; i++) {
+    free(c->ct[i]);
+  }
+  free(c->ct);
+  free(c);
 }
--- 33_counts/counts.h
+++ 33_counts/counts.h
@@ -2,16 +2,21 @@
 #define __COUNTS_H__
 struct _one_count_t {
   //DEFINE ME
-
+  char * value; 
+  size_t count; 
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
   //DEFINE ME
+  one_count_t ** ct; 
+  size_t length; 
+  size_t unknown_ct; 
 };
 typedef struct _counts_t counts_t;
 
 counts_t * createCounts(void);
+ssize_t find_name_idx(counts_t * c, const char * name); 
 void addCount(counts_t * c, const char * name);
 void printCounts(counts_t * c, FILE * outFile);
 
