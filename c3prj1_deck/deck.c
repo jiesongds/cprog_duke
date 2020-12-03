138f0891ec22a5ae511b38427bb543dc4eb33cf6
Jie Song <songjay8@gmail.com>
2020-12-03 17:34:16 +0800

    did prj1

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -28,21 +28,13 @@ int deck_contains(deck_t * d, card_t c) {
 void shuffle(deck_t * d){
   card_t **card_ptr = d->cards;
   int ncards = d->n_cards;
- 
- 
-  //size_t up = ncards-1;
 
-  //if (ncards % 2 ==0){
-    //ncards = ncards-1;
-    //}
-  
   for (int k=0; k < ncards; k++) {
     int rd_off = rand()%ncards;
     card_t *temp = card_ptr[k];
     card_ptr[k] = card_ptr[rd_off];
     card_ptr[rd_off] = temp;
     }
-   
 }
 
 void assert_full_deck(deck_t * d) {
@@ -67,3 +59,62 @@ void assert_full_deck(deck_t * d) {
     assert(count == 1);
   }
 }
+
+void add_card_to(deck_t * deck, card_t c) {
+  deck->cards = realloc(deck->cards, (deck->n_cards + 1) * sizeof(*(deck->cards)));
+  deck->cards[deck->n_cards] = malloc(sizeof(**(deck->cards)));
+  *(deck->cards[deck->n_cards]) = c; 
+  deck->n_cards = deck->n_cards + 1; 
+}
+
+card_t * add_empty_card(deck_t * deck) {
+  card_t c;
+  c.suit = 0;
+  c.value = 0; 
+  add_card_to(deck, c);
+  card_t * ptr = deck->cards[deck->n_cards-1]; 
+  return ptr; 
+}
+
+deck_t * make_deck_exclude(deck_t * excluded_cards) {
+  deck_t * dk; 
+  dk = malloc(sizeof(*dk)); 
+  dk->n_cards = 0;
+  dk->cards = NULL;
+  for (unsigned c=0; c<52; c++) {
+    card_t ct = card_from_num(c); 
+    if (!deck_contains(excluded_cards, ct)) {
+      add_card_to(dk, ct); 
+    }
+  }
+  return dk;
+}
+
+void free_deck(deck_t * deck) {
+  for (size_t i = 0; i<deck->n_cards; i++) {
+    free(deck->cards[i]);
+  }
+  free(deck->cards);
+  free(deck); 
+}
+
+deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands) {
+  deck_t * ex_dk;
+  ex_dk = malloc(sizeof(*ex_dk)); 
+  ex_dk->n_cards = 0;
+  ex_dk->cards = NULL;  
+
+  for (size_t i=0; i<n_hands; i++) {
+    deck_t * hand = hands[i];
+    for (size_t j = 0; j<hand->n_cards; j++) {
+      card_t c = *(hand->cards[j]);
+      add_card_to(ex_dk, c);
+    }
+  }
+
+  deck_t * dk = make_deck_exclude(ex_dk);
+  free_deck(ex_dk); 
+  return dk; 
+}
+
+
