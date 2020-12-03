5860863de660524229ebd5789b9016fae198a485
Jie Song <songjay8@gmail.com>
2020-12-03 23:51:17 +0800

    doing c4prj2

---
+++ c4prj2_input/future.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "future.h"
+
+void add_futher_card(future_cards_t * fc, size_t index, card_t * ptr) {
+    if (index >= fc->n_decks) {
+        fc->decks = realloc(fc->decks, (index+1) * sizeof(*(fc->decks))); 
+        fc->n_decks = index + 1; 
+        for (size_t i = 0; i<fc->n_decks; i++) {
+            if (fc->decks[i].n_cards > 0) {
+                continue;
+            }
+            else {
+                fc->decks[i].cards = NULL;
+                fc->decks[i].n_cards = 0;
+            }  
+        }
+    }
+    fc->decks[index].cards = realloc(fc->decks[index].cards, (fc->decks[index].n_cards+1) * sizeof(*(fc->decks[index].cards)));
+    fc->decks[index].cards[n_cards] = ptr; 
+    fc->decks[index].n_cards++; 
+}
+
+void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
+    for (size_t i = 0; i<fc->n_decks; i++) {
+       for (size_t j=0; j<fc->decks[i].n_cards; j++) {
+           fc->decks[i].cards[j]->value = deck->cards[i]->value;
+           fc->decks[i].cards[j]->suit = deck->cards[i]->suit;
+       } 
+    }
+}
---
+++ c4prj2_input/test-future.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "future.h"
+#include "deck.h"
+#include "cards.h"
+
+int main() {
+    future_cards_t * fc;
+    fc = malloc(sizeof(*fc));
+
+    deck_t * h1; 
+    h1 = malloc(*h1); 
+    h1->cards = NULL;
+    h1->n_cards = 0; 
+    add_empty_card(h1);
+    add_empty_card
+
+
+}
